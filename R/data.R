49bbd7266e484114c9dba48ae40b29745c579a36
Simon Cockell <sjcockell@gmail.com>
2018-10-10 15:46:34 +0100

    Initial Commit

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Raw counts for Gencode genes in 23 samples.
+#'
+#' A dataset containing the raw counts for 58243
+#' genes in 24 samples.
+#'
+#' @format A matrix with 58243 rows and 23 columns
+#'
+"counts"
+#' tximport results for Gencode genes in 23 samples.
+#'
+#' A dataset containing the raw counts, lengths and
+#' scaled counts for 58243 genes in 23 samples.
+#'
+#' @format A list of 4 objects, results of tximport
+#'
+"quant_table"
